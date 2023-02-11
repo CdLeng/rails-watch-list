a2ef6862cbe42a5bf58d395f557785b531df456f
Coen de Leng <coendeleng@outlook.com>
2023-02-11 17:47:57 +0100

    Image upload function

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,8 @@
+require 'erb'
+require 'ostruct'
+
 class ListsController < ApplicationController
+  helper_method :test, :get_binding
 
   def index
     @lists = List.all
@@ -27,9 +31,19 @@ class ListsController < ApplicationController
     redirect_to lists_path, status: :see_other
   end
 
+  def test(list)
+    @cl_image_path = "<%= cl_image_path list.photo.key %>"
+    @asset_path = "<%= asset_path('movie_theater.jpg') %>"
+    if list.photo.key
+      template = ERB.new(@cl_image_path)
+    else
+      ERB.new(@asset_path)
+    end
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name, :image_url)
+    params.require(:list).permit(:name, :photo)
   end
 end
